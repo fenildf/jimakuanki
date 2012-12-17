abd11a97473884fe1aee240b7866a2efb128ef7a
ospalh <ospalh@gmail.com>
2012-12-17 17:45:19 +0100

    Creating a collection works, just not selecting the deck.

---
+++ subtitledecker/plain_japanese_model.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2012 ©: Roland Sieker <ospalh@gmail.com>
+
+# Provenance: Based on anki.stdmodels.addBasicModel
+# by Damien Elmes <anki@ichi2.net>
+#
+# License: GNU AGPL, version 3 or later;
+# http://www.gnu.org/licenses/agpl.html
+
+from anki.lang import _
+
+def add_plain_japanese_model(col):
+    mm = col.models
+    m = mm.new(_('Subtitle deck'))
+    fm = mm.newField(_('Timestamp'))
+    mm.addField(m, fm)
+    fm = mm.newField(_('Meaning'))
+    mm.addField(m, fm)
+    fm = mm.newField(_('Reading'))
+    mm.addField(m, fm)
+    fm = mm.newField(_('Native'))
+    mm.addField(m, fm)
+    fm = mm.newField(_('Image'))
+    mm.addField(m, fm)
+    fm = mm.newField(_('Video'))
+    mm.addField(m, fm)
+    fm = mm.newField(_('Audio'))
+    mm.addField(m, fm)
+    fm = mm.newField(_('Literal'))
+    mm.addField(m, fm)
+    fm = mm.newField(_('Notes'))
+    mm.addField(m, fm)
+    t = mm.newTemplate(_('Watch'))
+    t['qfmt'] = '<div>{{' + _('Image') + '}}</div>\n{{'+_('Video')+'}}\n' \
+        + '<div class=small>{{' + _('Timestamp') + '}}</div>'
+    t['afmt'] = '{{' + _('Image') + '}}\n{{' + _('Audio')+ '}}\n' + \
+        '<div>{{furigana:'+_('Reading')+'}}</div>' + \
+        '<div class="native">{{'+_('Native')+'}}</div>' + \
+        '<div class="small">{{'+_('Timestamp')+'}}</div>'
+    mm.addTemplate(m, t)
+    mm.add(m)
+    return m
--- subtitledecker/subtitledecker.py
+++ subtitledecker/subtitledecker.py
@@ -5,7 +5,7 @@
 #
 # Provenance: Parts of the code were written by Damien Elmes, as
 # * anki.storage.Collection()
-# * ...
+# * aqt.profiles
 #
 # License: GNU AGPL, version 3 or later;
 # http://www.gnu.org/copyleft/agpl.html
@@ -14,6 +14,9 @@
 Turn a video and sets of subtitles into an Anki2 deck.
 """
 
+import random
+
+
 from anki.collection import _Collection
 from anki.storage import _createDB
 import os
@@ -21,6 +24,11 @@ import tempfile
 import shutil
 from anki.db import DB
 from anki.consts import SCHEMA_VERSION
+from anki.notes import Note
+from anki.decks import DeckManager
+
+from plain_japanese_model import add_plain_japanese_model
+
 
 class SubtitleDecker(object):
     """
@@ -32,7 +40,12 @@ class SubtitleDecker(object):
         self.args = args
         self.col_path = ''
         self.col_name = 'collection'
+        self.model = None
         self.col = self.get_collection()
+        self.dm = self.col.decks
+        self.deck_id = self.dm.id("Video deck")
+        self.dm.select(self.deck_id)
+        self.col.models.setCurrent(self.model)
 
     def get_collection(self):
     #def Collection(path, lock=True, server=False, sync=True):
@@ -45,25 +58,27 @@ class SubtitleDecker(object):
         # directory to put these media files.
         self.col_path = tempfile.mkdtemp()
         db = DB(os.path.join(self.col_path, self.col_name + ".anki2"))
-        ver = _createDB(db)
+        _createDB(db)
         db.execute("pragma temp_store = memory")
-        db.execute("pragma cache_size = 10000")
-        db.execute("pragma journal_mode = wal")
-        if ver < SCHEMA_VERSION:
-            _upgrade(col, ver)
-
-    # add db to col and do any remaining upgrades
+        db.execute("pragma synchronous = off")
+        # add db to col and do any remaining upgrades
         col = _Collection(db, server=False)
+        self.model = add_plain_japanese_model(col)
         # add in reverse order so basic is default
         # addClozeModel(col)
         # addForwardOptionalReverse(col)
         # addForwardReverse(col)
         # addBasicModel(col)
+        col.save()
         col.lock()
         return col
 
     def process(self):
+        note = Note(self.col, model=self.model)
+        note['Timestamp'] = "007"
+        self.col.addNote(note)
         pass
+
 
     def rm_temp_collection(self):
         self.col.close()
