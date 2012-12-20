aa2bb682497d3ba7f53f4992c18377bb374bce0e
ospalh <ospalh@gmail.com>
2012-12-20 09:26:12 +0100

    setup with just distutils

---
+++ setup.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 ; mode: python -*-
+# © 2012 ospalh
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
+from distutils.core import setup
+
+setup(
+    name="subtitle-decker",
+    version="0.1a1",
+    description="""Script to turn video and subtitle files into Anki2 \
+SRS decks. """,
+    packages=['subtiledecker'],
+    author="Roland Sieker",
+    author_email="ospalh@gmail.com",
+    )
