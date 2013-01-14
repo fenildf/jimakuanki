0d9ce7ee69df5d7fdf1b11d1bca16f364712e32a
ospalh <ospalh@gmail.com>
2013-01-14 11:21:11 +0100

    Reanmed, use the renamed file.

--- jimakuanki/jimakuanki.py
+++ jimakuanki/jimakuanki.py
@@ -27,7 +27,7 @@ from libanki.notes import Note
 from libanki.storage import _createDB
 from libanki.lang import _
 
-from .model import add_simple_model
+from . import models
 
 # How many times will we try to
 fudge_timing_max_frames = 10
@@ -58,7 +58,14 @@ class JimakuAnki(object):
         self.deck_id = None
         self.subtitle_files = []
         self.subtitles = []
-        self.time_sub_index = 0
+        # Always use the first (0th) subtitle for timing.
+        # self.time_sub_index = 0
+        self.matched_subtitles = []
+        self.leading_lines = 2
+        self.trailing_lines = 2
+        # Should we add local translations of leading and trailing
+        # lines as well?
+        self.lead_trail_local = True
         self.video = None
         self.model_name = None
         self.language_name = u'Expression'
@@ -72,10 +79,15 @@ class JimakuAnki(object):
         self.col = self.get_collection()
         self.dm = self.col.decks
 
-    def add_model(self):
-        self.model = add_simple_model(
+    def add_simple_model(self):
+        self.model = models.add_simple_model(
             self.col, self.model_name, self.language_name,
             self.native_language_name)
+        self.col.models.setCurrent(self.model)
+        self.model['did'] = self.deck_id
+
+    def add_dynamic_model(self):
+        self.model = models.add_dynamic_model(self)
         self.col.models.setCurrent(self.model)
         self.model['did'] = self.deck_id
 
@@ -145,12 +157,16 @@ class JimakuAnki(object):
                         subs.from_str(fp.read())
             self.subtitles.append(subs)
 
+    def _arrange_titles():
+        pass
+
     def process(self):
         self._get_subtitles_from_files()
         if not self.subtitles:
             print('No subtitles loaded')
             return
         time_sub = self.subtitles[self.time_sub_index]
+
         # Need to avoid code duplication.
         for sl in time_sub:
             note = Note(self.col, model=self.model)
--- jimakuanki/models.py
+++ jimakuanki/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2012 ©: Roland Sieker <ospalh@gmail.com>
+# Copyright © 2012 : Roland Sieker <ospalh@gmail.com>
 
 # Provenance: Based on anki.stdmodels.addBasicModel
 # by Damien Elmes <anki@ichi2.net>
@@ -36,6 +36,68 @@ def add_simple_model(col, name, language_name, native_language_name):
     fm = mm.newField(reading_name)
     mm.addField(m, fm)
     fm = mm.newField(native_language_name or _(u'Meaning'))
+    mm.addField(m, fm)
+    image_name = _(u'Image')
+    fm = mm.newField(image_name)
+    mm.addField(m, fm)
+    video_name = _(u'Video')
+    fm = mm.newField(video_name)
+    mm.addField(m, fm)
+    audio_name = _(u'Audio')
+    fm = mm.newField(audio_name)
+    mm.addField(m, fm)
+    fm = mm.newField(_(u'Literal'))
+    mm.addField(m, fm)
+    fm = mm.newField(_(u'Notes'))
+    mm.addField(m, fm)
+    t = mm.newTemplate(_(u'Watch'))
+    # NB.: those things are five pairs of braces: double braces are
+    # escapes for single braces, so four of the five give double
+    # braces, what we want in the output. The last pair is used by
+    # str.format().
+    # The character between start and end is an n-dash.
+    t['qfmt'] = u'''\
+{{{{{image}}}}}
+{{{{{video}}}}}
+<div class="small">{{{{{st}}}}}–{{{{{end}}}}}</div>
+'''.format(image=image_name, video=video_name, st=start_name, end=end_name)
+
+    t['afmt'] = u'''\
+{{{{{image}}}}}
+{{{{{audio}}}}}
+<div>{{{{{text}}}}}</div>
+<div>{{{{furigana:{read}}}}}</div>
+<div>{{{{{native}}}}}</div>
+<div class="small">{{{{{st}}}}}–{{{{{end}}}}}</div>
+'''.format(image=image_name, audio=audio_name, text=language_name,
+           read=reading_name, native=native_language_name, st=start_name,
+           end=end_name)
+    # Arial is ugly.
+    if remove_arial:
+        m['css'] = m['css'].replace(
+            'font-family: arial;', '/* font-family: arial; */')
+    m['css'] += extra_styling
+    mm.addTemplate(m, t)
+    mm.add(m)
+    return m
+
+
+def add_dynamic_model(makuan):
+    mm = makuan.col.models
+    m = mm.new(makuan.model_name or _('Subtitles'))
+
+    start_name = _('Start')
+    end_name = _('End')
+    fm = mm.newField(start_name)
+    mm.addField(m, fm)
+    fm = mm.newField(end_name)
+    mm.addField(m, fm)
+    fm = mm.newField(makuan.language_name)
+    mm.addField(m, fm)
+    reading_name = _(u'Reading')
+    fm = mm.newField(reading_name)
+    mm.addField(m, fm)
+    fm = mm.newField(makuan.native_language_name or _(u'Meaning'))
     mm.addField(m, fm)
     image_name = _(u'Image')
     fm = mm.newField(image_name)
